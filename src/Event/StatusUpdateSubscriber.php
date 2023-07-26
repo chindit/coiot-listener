813044cd846442bacfbcb8f93c83f9f061269f39
chindit <littletiger58@gmail.com>
2023-07-26 22:22:03 +0200

    🚧 Basic RPC event

--- src/Event/StatusUpdateSubscriber.php
+++ src/Event/StatusUpdateSubscriber.php
@@ -2,23 +2,31 @@
 
 namespace App\Event;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class StatusUpdateSubscriber implements EventSubscriberInterface
 {
-
+    public function __construct(private readonly EntityManagerInterface $entityManager){}
     /**
      * @inheritDoc
      */
     public static function getSubscribedEvents()
     {
         return [
-            StatusUpdateEvent::NAME => 'onUpdate'
+            StatusUpdateEvent::NAME => 'onUpdate',
+            StatusUpdateRpcEvent::NAME => 'onRpcUpdate'
         ];
     }
 
     public function onUpdate(StatusUpdateEvent $event):void
     {
         dump($event);
+    }
+
+    public function onRpcUpdate(StatusUpdateRpcEvent $event): void
+    {
+        $this->entityManager->persist($event->shelly);
+        $this->entityManager->flush();
     }
 }
