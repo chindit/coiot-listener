8a6a9340fad1845d4de28c7359121fd1b83d621b
chindit <littletiger58@gmail.com>
2023-08-06 20:04:18 +0200

    🐛 Handle JSON char in CoIoT payload

--- src/Command/CoIoTListener.php
+++ src/Command/CoIoTListener.php
@@ -53,7 +53,7 @@ class CoIoTListener extends Command {
     private function parseCoIoT(string $input): void
     {
         $utf8String = preg_replace('/\s+/', '', mb_convert_encoding($input, 'UTF-8', 'ASCII'));
-        $pattern = '/(#(.*)#).*({.*})/mU';
+        $pattern = '/(#(.*)#).*({".*})/mU';
         preg_match($pattern, $utf8String, $matches);
 
         if (count($matches) === 4) {
