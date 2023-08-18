f740f0453a7eb07608ae449ab9863f01ffbe718c
chindit <littletiger58@gmail.com>
2023-08-18 10:12:24 +0200

    Fix path

--- src/Command/CoIoTListener.php
+++ src/Command/CoIoTListener.php
@@ -104,12 +104,12 @@ class CoIoTListener extends Command {
                 ]);
 
             $this->dispatcher->dispatch(new StatusUpdateRpcEvent($shellyStatus), StatusUpdateRpcEvent::NAME);
-        } elseif ($payload?->method === 'NotifyStatus' && $payload?->method?->params?->switch?->current) {
+        } elseif ($payload?->method === 'NotifyStatus' && $payload?->params?->switch?->current) {
 
             $shellyStatus = (new Shelly())
                 ->setDeviceId(explode('-', $payload->src)[1])
                 ->setType('plug')
-                ->setPower($payload?->method?->params?->switch?->current)
+                ->setPower($payload?->params?->switch?->current)
                 ->setTemperature(0.0);
 
             $this->dispatcher->dispatch(new StatusUpdateRpcEvent($shellyStatus), StatusUpdateRpcEvent::NAME);
